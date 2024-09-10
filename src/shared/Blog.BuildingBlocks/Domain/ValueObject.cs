1c5f93abf77c65ea90d69eaafd579419926d2c3b
alirezadoostdar <alireza.doostdar@gmail.com>
2024-09-10 23:36:52 +0330

    add ValueObject class to buildingBlocks project

---
+++ src/shared/Blog.BuildingBlocks/Domain/ValueObject.cs
@@ -0,0 +1,25 @@
+﻿namespace Blog.BuildingBlocks.Domain;
+
+public abstract class ValueObject<T> where T : ValueObject<T>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+        => obj is not null &&
+        obj is T valuObject &&
+        obj.GetType() == GetType() &&
+        GetEqualityComponents().SequenceEqual(valuObject.GetEqualityComponents());
+
+    public static bool operator ==(ValueObject<T> left, ValueObject<T> right)
+        =>left.Equals(right);
+
+    public static bool operator !=(ValueObject<T> left, ValueObject<T> right)
+    => !left.Equals(right);
+
+    public override int GetHashCode()
+        => GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x , y) => x ^ y);
+
+}
+
