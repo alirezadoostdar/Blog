1dbe3f82959e0b975967086d1f3f28af924dddf4
alirezadoostdar <alireza.doostdar@gmail.com>
2024-09-13 22:57:30 +0330

    Add Tag feature

---
+++ src/Bolog.Domain/ArticleAggregate/Models/Tag.cs
@@ -0,0 +1,28 @@
+﻿using Humanizer;
+using Blog.BuildingBlocks.Domain;
+
+namespace Bolog.Domain.ArticleAggregate.Models;
+
+public class Tag : ValueObject<Tag>
+{
+    public string Value { get; set; } = null!;
+
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    public static Tag Create(string tagValue)
+    {
+        return new Tag
+        {
+            Value = tagValue.Kebaberize()
+        };
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
