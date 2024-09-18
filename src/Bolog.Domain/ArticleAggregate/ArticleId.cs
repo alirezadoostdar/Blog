48eec7a7483c55c2aa87aa79fd58e15038f902e5
alirezadoostdar <alireza.doostdar@gmail.com>
2024-09-18 23:02:19 +0330

    Add ArticleId

---
+++ src/Bolog.Domain/ArticleAggregate/ArticleId.cs
@@ -0,0 +1,22 @@
+﻿using Blog.BuildingBlocks.Domain;
+using Humanizer;
+
+namespace Bolog.Domain.ArticleAggregate;
+
+public sealed class ArticleId : ValueObject<ArticleId>
+{
+    public required string Slug { get; set; }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Slug;
+    }
+
+    public static ArticleId CreateUniqueId(string title)
+        =>Create(title.Kebaberize());
+
+    public static ArticleId Create(string title)
+        =>new ArticleId { Slug = title };
+
+    public override string ToString() => Slug;
+}
