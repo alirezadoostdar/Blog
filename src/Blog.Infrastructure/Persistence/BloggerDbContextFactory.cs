240ea9203f6defd7375e66d3f3a3eda09ba5728b
alirezadoostdar <alireza.doostdar@gmail.com>
2024-10-01 12:59:49 +0200

    add db context factory

---
+++ src/Blog.Infrastructure/Persistence/BloggerDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Blog.Infrastructure.Persistence;
+
+public class BloggerDbContextFactory : IDesignTimeDbContextFactory<BloggerDbContext>
+{
+    public BloggerDbContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<BloggerDbContext>();
+        optionBuilder.UseSqlServer("data source=sql2019;initial catalog=blogger;TrustServerCertificate=True;Trusted_Connection=True;");
+
+        return new BloggerDbContext(optionBuilder.Options);
+    }
+}
